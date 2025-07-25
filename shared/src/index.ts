9f91f20f1ed34f43659cf53da5d98dcbd40df5da
ammar.allahham <ammar.lm96@gmail.com>
2025-07-25 23:00:28 +0400

    init shared

---
+++ shared/src/index.ts
@@ -0,0 +1,15 @@
+export * from './database/database.module';
+
+export * from './database/abstract.repository';
+
+export * from './entities/program.entity';
+export * from './entities/abstract.entity';
+
+export * from './dto/pagination.dto';
+
+export * from './types/type';
+
+export * from './config/validation';
+
+export * from './repository/content.repository';
+export * from './repository/shared.repository.module';
